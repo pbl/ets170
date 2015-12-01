ae7c02cc54a93095bf3da283ccda450fd7b8a7fe
pbl <philip.burenstam.linder@gmail.com>
2015-12-01 10:53:15 +0100

    15 req and 2 stakholders from group g project

---
+++ lab2/req.scala
@@ -0,0 +1,18 @@
+Model (
+	Req (" liveStream ") ,
+	Req (" commentsSection ") ,
+	Req (" lectureRating ") ,
+	Req (" liveStreamQuestion ") ,
+	Req (" playVideo ") ,
+	Req (" courseRating ") ,
+	Req (" lectureSlides ") ,
+	Req (" stilLogin ") ,
+	Req (" lucatLogin ") ,
+	Req (" liveStreamChat ") ,
+	Req (" courseInfo ") ,
+	Req (" coursePage ") ,
+	Req (" courseRatingDisplay ") ,
+	Req (" lectureRatingDisplay ") ,
+	Req (" adminLogin ") ,
+	Stakeholder (" lecturer ") ,
+	Stakeholder (" student ") )
