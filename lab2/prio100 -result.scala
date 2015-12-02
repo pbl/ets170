bd1481cc4235837122cbfff101cd2e1d8db86a67
pbl <philip.burenstam.linder@gmail.com>
2015-12-02 13:54:47 +0100

    2.1.1 complete

---
+++ lab2/prio100 -result.scala
@@ -0,0 +1,16 @@
+Model(
+  Feature("liveStream") has Prio(3), 
+  Feature("commentsSection") has Prio(4), 
+  Feature("lectureRating") has Prio(12), 
+  Feature("liveStreamQuestion") has Prio(1), 
+  Feature("playVideo") has Prio(12), 
+  Feature("courseRating") has Prio(12), 
+  Feature("lectureSlides") has Prio(8), 
+  Feature("stilLogin") has Prio(10), 
+  Feature("lucatLogin") has Prio(10), 
+  Feature("liveStreamChat") has Prio(2), 
+  Feature("courseInfo") has Prio(4), 
+  Feature("coursePage") has Prio(10), 
+  Feature("courseRatingDisplay") has Prio(4), 
+  Feature("lectureRatingDisplay") has Prio(4), 
+  Feature("adminLogin") has Prio(3))
